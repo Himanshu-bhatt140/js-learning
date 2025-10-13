1228597090d9088dfd124937745eca46177e5e0b
Himanshu-bhatt140 <himanshubhatt140@gmail.com>
2025-10-13 07:18:48 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// "use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using node js so it will not pop up on node it will work on browsers and its console
+// console.log(3+3); 
+// console.log("himanshu");
+
+// 1. Primitive DataType
+// number  => 2 to power 53
+// bigint => if the number is bigger that number ranger usually for big applications like reddit,facebook any stock or trading app
+// string => ""
+// boolean => true or false
+// null => standalone value => representation of empty value
+// undefined => value is  not defined but the variable is declared
+// symbol => used for finding uniqueness 
+
+// 2. Non primitive datatypes
+//  Objects
+
+// console.log(typeof "himanshu"); => string
+// console.log(typeof null); => object
+// console.log(typeof undefined); => undefined
+
--- 01_basics/02_dataypes.js
+++
@@ -1,6 +0,0 @@
-// "use strict"; // treat all JS code as newer version
-
-// alert(3 + 3)  // we are using node js so it will not pop up on node it will work on browsers and its console
-console.log(3+3); 
-console.log("himanshu");
-
