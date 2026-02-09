af5fe2e044182f0023b4f776ebe5d01e91090e99
Himanshu-bhatt140 <himanshubhatt140@gmail.com>
2026-02-09 06:47:47 +0530

    control flow

--- controlFlow/ifElse.js
+++ controlFlow/ifElse.js
@@ -1,8 +1,22 @@
-const isLoggedIn=false;
-if (isLoggedIn===true) {
-    console.log("User looged in");
+// const isLoggedIn=false;
+// if (isLoggedIn===true) {
+//     console.log("User looged in");
     
-}else{
-    console.log("login first");
+// }else{
+//     console.log("login first");
+    
+// }
+
+const userLoggedIn =true;
+const debitCard=true;
+const isLoggedInFromGoogle=true;
+const isLoggedInFromEmail=true;
+
+if(userLoggedIn && debitCard){
+    console.log(`Allowed to Shop`);
+    
+}
+ if(isLoggedInFromGoogle || isLoggedInFromEmail){
+    console.log(`User Logged in & allowed to proceed`);
     
 }
---
+++ controlFlow/switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+// case 4:
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("Default case matched");
+//         break;
+// }
+
+// If the break is not used then it will exeecuted till end till the default or the last where the break has been added
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("not matched");
+        break;
+}
