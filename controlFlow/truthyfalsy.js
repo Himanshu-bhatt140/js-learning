d22b887f2db65efa35cf4004cfa3d6b6cce3d082
Himanshu-bhatt140 <himanshubhatt140@gmail.com>
2026-02-09 07:37:14 +0530

    thruthy falsy

---
+++ controlFlow/truthyfalsy.js
@@ -0,0 +1,33 @@
+const email =[]
+if (email) {
+    // console.log("got user email");
+
+} else {
+    // console.log("don't have email");
+
+}
+
+// Falsy values => the values which are assumed to be false
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+// Truthy values => values which are assumed to be true
+// "0","false"," ",[],{},function(){}
+
+// check if array is empthy or not
+if(email.length===0){
+    // console.log(`array is empty`);
+    
+}
+
+// Nullish Coalescing operator (??) : null, undefined
+
+let val1;
+// val1=10 ?? 15; => 10
+// val1=null ?? 15; => 15
+// val1=undefined ?? null ?? 25;  ==> 25
+// val1=undefined ?? null ?? 25;  ==> 25
+
+
+
+
+console.log(val1);
