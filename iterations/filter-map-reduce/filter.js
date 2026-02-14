fa0cdaa59c17f8f766bf7e77f434ddf2c2fbc61f
Himanshu-bhatt140 <himanshubhatt140@gmail.com>
2026-02-14 10:52:05 +0530

    filter

--- iterations/filter-map-reduce/filter.js
+++ iterations/filter-map-reduce/filter.js
@@ -22,5 +22,50 @@ const numFor = myNum.forEach( (n) => {
         
     }
 } )
-console.log(newNum);
+// console.log(newNum);
 
+// Filter Example
+
+const books = [
+    {
+        title:"Book One", genre:"Fiction",publish:1981,edition:2004
+    },
+    {
+        title:"Book Two", genre:"Non-Fiction",publish:1982,edition:2008
+    },
+    {
+        title:"Book Three", genre:"History",publish:1999,edition:2009
+    },
+    {
+        title:"Book Four", genre:"Non-Fiction",publish:1989,edition:2010
+    },
+    {
+        title:"Book Five", genre:"Science",publish:2009,edition:2012
+    },
+    {
+        title:"Book Six", genre:"Fiction",publish:1987,edition:2006
+    },
+    {
+        title:"Book Seven", genre:"History",publish:1983,edition:1999
+    },
+    {
+        title:"Book Eight", genre:"Science",publish:1971,edition:2004
+    },
+]
+
+// I want the books whose publish year is before 1980
+
+// const bookfilter = books.filter((bk) =>   bk.genre==="History")
+
+
+// const bookFilter = books.filter((bk)=>{
+//     return bk.genre === "Fiction"
+// })
+
+// I want the books published befor 1995 and the genre should be Non-Fiction
+
+const bookFilter= books.filter((bk)=>{
+    return bk.publish <= 1995 && bk.genre==="Non-Fiction"
+})
+
+console.log(bookFilter);
